3ffbf1cb452a4fc2db15f5f986c7ec9ad77b416c
wallacefcosta <wallacebassline@gmail.com>
2023-05-16 22:35:02 -0300

    First Crud

---
+++ README.md
@@ -0,0 +1,30 @@
+# projetoFinalBackend Readme
+## Tabela de Conteúdos
+- [Sobre](#sobre)
+- [Começando](#começando)
+- [Uso](#uso)
+
+ ## Sobre <a name = "sobre"></a>
+Este projeto é parte final do modulo de introdução ao backend do curso de desenvolvimento WebFullStack Developer da Growdev. O objetivo deste projeto é criar uma API utilizando o NodeJs, que permita aos usuários criar, ler, atualizar e excluir dados.
+ ## Começando <a name = "começando"></a>
+Estas instruções irão te guiar para ter uma cópia do projeto funcionando em sua máquina local para propósitos de desenvolvimento e teste.
+ ### Pré-requisitos
+Antes de começar, certifique-se de ter instalado:
+- Node.js (v14 ou mais recente)
+- npm ou yarn
+ ### Instalação
+Primeiro, clone o repositório:
+git clone https://github.com/wallacefcosta/projetoFinalBackend.git
+Em seguida, navegue até o diretório do projeto:
+cd projetoFinalBackend
+Depois, instale as dependências:
+npm install
+Ou, se você estiver usando o yarn:
+yarn
+### Uso <a name = "uso"></a>
+Para iniciar o servidor, execute:
+npm start
+Ou, se você estiver usando o yarn:
+yarn start
+O servidor estará funcionando em  `http://localhost:8080` .
+Para testar a API, você pode usar ferramentas como [Postman](https://www.postman.com/) ou [Insomnia](https://insomnia.rest/).
---
+++ src/index.js
@@ -0,0 +1,165 @@
+import express from "express";
+
+const app = express();
+const crypto = require('node:crypto')
+app.use(express.json());
+
+
+app.get("/", (request, response) => {
+  return response.json(`Olá, seja bem vindo ao seu CRUD de recados.`);
+});
+//Criar e listar usuarios
+const listaUsuario = [];
+
+app.get("/list-users", (request, response) => {
+  return response.json(listaUsuario);
+});
+app.post(`/user`, (request, response) => {
+  const dados = request.body;
+
+  const novoUsuario = {
+    id: crypto.randomUUID(),
+    nome: dados.nome,
+    email: dados.email,
+    password: dados.password,
+  };
+  const existe = listaUsuario.some((user) => user.email === novoUsuario.email);
+  if (existe) {
+    return response.status(400).json({
+      sucesso: false,
+      dados: null,
+      mensagem: "Outro usuário já está cadastrado com este e-mail.",
+    });
+  }
+  if (!dados.password || dados.password.length < 6) {
+    return response.status(400).json({
+      sucesso: false,
+      dados: null,
+      mensagem:
+        "É obrigatório informar a senha para cadastro do usuário com no mínimo 6 caracteres",
+    });
+  } else if (
+    !dados.email ||
+    !dados.email.includes("@") ||
+    !dados.email.includes(".com")
+  ) {
+    return response.status(400).json({
+      sucesso: false,
+      dados: null,
+      mensagem:
+        "É obrigatório informar um e-mail válido para cadastro do usuário",
+    });
+  }
+  listaUsuario.push(novoUsuario);
+
+  return response.status(201).json({
+    succes: true,
+    message: `Usuário criado com sucesso`,
+    data: novoUsuario,
+  });
+});
+
+app.post(`/login`, (request, response) => {
+  const dadosDoUsuario = request.body;
+
+  const emailCorreto = listaUsuario.some(
+    (user) => user.email === dadosDoUsuario.email
+  );
+
+  const senhaCorreta = listaUsuario.some(
+    (user) => user.password === dadosDoUsuario.password
+  );
+
+  if (!emailCorreto || !senhaCorreta) {
+    return response.status(400).json({
+      success: false,
+      message: `Email ou senha incorretos`,
+      data: {},
+    });
+  }
+
+  listaUsuario.forEach((usuario) => (usuario.logado = false));
+
+  const user = listaUsuario.find((user) => user.email === dadosDoUsuario.email);
+
+  user.logado = true;
+
+  return response.json({
+    success: true,
+    message: `Usuário logado com sucesso`,
+    data: {},
+  });
+});
+
+const listaRecados = [];
+
+app.get("/list-recados", (request, response) => {
+  return response.json(listaRecados);
+});
+
+app.post(`/recados`, (request, response) => {
+  const dados = request.body;
+
+  const usuario = listaUsuario.find((user) => user.logado === true);
+
+  if (!usuario) {
+    return response.status(400).json({
+      success: false,
+      message: `Necessario fazer login para criar um recado`,
+      data: {},
+    });
+  }
+
+  const novoRecado = {
+    id: crypto.randomUUID(),
+    titulo: dados.titulo,
+    descricao: dados.descricao,
+    autor: usuario,
+  };
+
+  listaRecados.push(novoRecado);
+
+  return response.status(201).json({
+    success: true,
+    message: `Recado criado com sucesso`,
+    data: novoRecado,
+  });
+});
+
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const { titulo, descricao } = request.body;
+  const userIndex = listaRecados.findIndex((user) => user.id === id);
+
+  if (userIndex > 0) {
+    return response.status(400).json({ error: "Recado nao encontrado" });
+  }
+
+  const user = {
+    id,
+    descricao,
+  };
+  listaRecados[userIndex] = user;
+
+  return response.json(listaRecados);
+});
+
+app.delete(`/recados/:id`, (request, response) => {
+  const params = request.params;
+
+  const recadoExiste = listaRecados.findIndex(
+    (recado) => recado.id === params.id
+  );
+
+  if (recadoExiste > 0) {
+    return response.status(400).json(`Recado nao encontrado`);
+  }
+
+  listaRecados.splice(recadoExiste, 1);
+
+  console.log(listaRecados);
+
+  return response.json(`Recado deletado com sucesso`);
+});
+
+app.listen(8080, () => console.log(`🚀Server Online🚀`));
